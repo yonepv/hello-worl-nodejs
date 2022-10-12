88c61d2435628af02624931619e1a0c0513f6fd4
Rocio Philipps <yonepv@hotmail.com>
2022-10-12 10:58:53 -0400

    working on pipelines templates
    Signed-off-by: Rocio Philipps <yonepv@hotmail.com>

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+import json
+
+
+import json
+
+def lambda_handler(event, context):
+    #TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from lambda!')
+    }
