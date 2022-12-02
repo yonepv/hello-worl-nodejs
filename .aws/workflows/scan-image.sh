60752177ded7a616b92555834492b1318662043f
Rocio Philipps <yonepv@hotmail.com>
2022-12-02 09:24:56 -0500

    adjusting script for ecr scan image results
    Signed-off-by: Rocio Philipps <yonepv@hotmail.com>

--- .aws/workflows/scan-image.sh
+++ .aws/workflows/scan-image.sh
@@ -17,11 +17,13 @@ if [ $(echo $?) -eq 0 ]; then
     if [ "$critical" != null ] || [ "$high" != null ]; then
         echo "Docker image contains vulnerabilities at CRITICAL or HIGH level"
         echo $scan_results
-        echo "Repo: $repository_name, image_tag: $image_tag, scan_results: $scan_results" >> scan-results.out
+        echo "Repo: $repository_name, image_tag: $image_tag, scan_results: $scan_results" >> scan-results-$image_tag.out
         # if you want to delete the pushed image from container registry
         # aws ecr batch-delete-image --repository-name "$repository_name" --image-ids imageTag="$image_tag"
-        exit 1
+        #exit 1
+        export CODEBUILD_BUILD_SUCCEEDING=0
     else
-        echo "Repo: $repository_name, image_tag: $image_tag, scan_results: No critical or high level vulnerabilities found" >> scan-results.out
-      fi
+        echo "No critical or high level vulnerabilities found"
+        #echo "Repo: $repository_name, image_tag: $image_tag, scan_results: No critical or high level vulnerabilities found" >> scan-results-$image_tag.out
+    fi
 fi
